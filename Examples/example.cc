9d6591fdbdfcccbd455a016145ebd4793b325ec7
Joshua Cates <hammerhead810@gmail.com>
2024-07-27 21:10:25 -0400

    :books: Updated example

--- Examples/example.cc
+++ Examples/example.cc
@@ -1,28 +1,19 @@
-/* Example circuit with three nodes. */
-#include "resistive.h"
+#include "../resistive.h"
 
 int
 main ()
 {
-	unsigned int nodes = 3; /* Number of nodes */
-	double g[9] = {1, -1, 0, -1, 3, -1, 0, -1, 2}; /* Conductance matrix */
-	double vIn = 5; /* Input source voltage */
-	unsigned int in = 1; /* Source is connected to node 1 */
+	Circuit c (0);
 
-	/* Create a DC circuit */
-	Circuit testCircuit (0);
+	c.addSource (1, 0, 5);
 
-	testCircuit.addSource (1, 0, 5);
-	testCircuit.addResistor (1, 2, 1);
-	testCircuit.addResistor (2, 0, 1);
-	testCircuit.addResistor (2, 3, 1);
-	testCircuit.addResistor (3, 0, 1);
+	c.addResistor (1, 2, 1);
+	c.addResistor (2, 0, 1);
+	c.addResistor (2, 3, 1);
+	c.addResistor (3, 0, 1);
 
-	/* Calculate the node voltages */
-	testCircuit.calcNodeVoltages ();
-
-	/* Print the node voltages */
-	testCircuit.printNodeVoltages ();
+	c.calcNodeVoltages ();
+	c.printNodeVoltages ();
 
 	return 0;
 }
